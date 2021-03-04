489494e371892a4ac49c49e5997b3a8fbf9f93f4
Sascha Steinbiss <satta@debian.org>
2021-03-04 22:55:15 +0100

    make importer more robust when VAST node crashes

--- importer/importer.go
+++ importer/importer.go
@@ -36,7 +36,7 @@ func MakeImporter(inChan chan []byte, name string, vastPath string, params []str
 func (i *Importer) Run(importType string) error {
 	i.Logger.Debugf("importer started")
 	for {
-		stopChan := make(chan bool)
+		stopChan := make(chan bool, 2)
 		params := append(i.Params, "import", importType)
 		i.Logger.Debugf("starting command '%s' with params %v", i.VastPath, params)
 		cmd := exec.Command(i.VastPath, params...)
@@ -52,23 +52,19 @@ func (i *Importer) Run(importType string) error {
 		if err != nil {
 			log.Fatal(err)
 		}
-		err = cmd.Start()
-		if err != nil {
-			log.Fatal(err)
-		}
 		stdinWriter := bufio.NewWriter(stdin)
-		go func() {
+		go func(sChan chan bool) {
 			i.Logger.Debug("started stdin writer goroutine")
 			defer i.Logger.Debug("left stdin writer goroutine")
 			for {
 				for line := range i.InChan {
-					i.CountLock.Lock()
-					i.Count++
-					i.CountLock.Unlock()
 					select {
-					case <-stopChan:
+					case <-sChan:
 						return
 					default:
+						i.CountLock.Lock()
+						i.Count++
+						i.CountLock.Unlock()
 						_, err := stdinWriter.Write(line)
 						if err != nil {
 							i.Logger.Debugf("could not write: %s", error(err))
@@ -76,49 +72,84 @@ func (i *Importer) Run(importType string) error {
 					}
 				}
 			}
-		}()
-		stderrReader := bufio.NewReader(stderr)
-		go func() {
-			for {
-				i.Logger.Debug("started stderr scanner goroutine")
-				scanner := bufio.NewScanner(stderrReader)
-				for scanner.Scan() {
-					i.Logger.Info(scanner.Text())
-				}
-				if err := scanner.Err(); err != nil {
-					i.Logger.Errorf("error reading stderr: %s", err)
-				}
-				i.Logger.Debug("end of stderr scanner goroutine")
-			}
-		}()
+		}(stopChan)
 		stdoutReader := bufio.NewReader(stdout)
-		go func() {
+		go func(sChan chan bool) {
+			i.Logger.Debug("started stdout scanner goroutine")
+			defer i.Logger.Debug("left stdout scanner goroutine")
 			for {
-				i.Logger.Debug("started stdout scanner goroutine")
-				scanner := bufio.NewScanner(stdoutReader)
-				for scanner.Scan() {
-					i.Logger.Info(scanner.Text())
+				select {
+				case <-sChan:
+					return
+				default:
+					i.Logger.Debug("started stdout scanner loop")
+					scanner := bufio.NewScanner(stdoutReader)
+					for scanner.Scan() {
+						select {
+						case <-sChan:
+							return
+						default:
+							i.Logger.Info(scanner.Text())
+						}
+					}
+					if err := scanner.Err(); err != nil {
+						i.Logger.Errorf("error reading stdout: %s", err)
+					}
+					i.Logger.Debug("end of stdout scanner loop")
 				}
-				if err := scanner.Err(); err != nil {
-					i.Logger.Errorf("error reading stdout: %s", err)
-				}
-				i.Logger.Debug("end of stdout scanner goroutine")
 			}
-		}()
-		go func() {
+		}(stopChan)
+		stderrReader := bufio.NewReader(stderr)
+		go func(sChan chan bool) {
+			i.Logger.Debug("started stderr scanner goroutine")
+			defer i.Logger.Debug("left stderr scanner goroutine")
 			for {
-				time.Sleep(time.Duration(i.CountIntervalSecs) * time.Second)
-				i.CountLock.Lock()
-				myCount := i.Count
-				i.Count = 0
-				i.CountLock.Unlock()
-				i.Logger.Infof("processed %f lines per second (%d total)", float64(myCount)/float64(i.CountIntervalSecs), myCount)
+				select {
+				case <-sChan:
+					return
+				default:
+					i.Logger.Debug("started stderr scanner loop")
+					scanner := bufio.NewScanner(stderrReader)
+					for scanner.Scan() {
+						select {
+						case <-sChan:
+							return
+						default:
+							i.Logger.Info(scanner.Text())
+						}
+					}
+					if err := scanner.Err(); err != nil {
+						i.Logger.Errorf("error reading stderr: %s", err)
+					}
+					i.Logger.Debug("end of stderr scanner loop")
+				}
 			}
-		}()
+		}(stopChan)
+		go func(sChan chan bool) {
+			i.Logger.Debug("started logger goroutine")
+			defer i.Logger.Debug("left logger goroutine")
+			for {
+				select {
+				case <-sChan:
+					return
+				case <-time.After(time.Duration(i.CountIntervalSecs) * time.Second):
+					i.CountLock.Lock()
+					myCount := i.Count
+					i.Count = 0
+					i.CountLock.Unlock()
+					i.Logger.Infof("processed %f lines per second (%d total)", float64(myCount)/float64(i.CountIntervalSecs), myCount)
+				}
+			}
+		}(stopChan)
+		err = cmd.Start()
+		if err != nil {
+			log.Fatal(err)
+		}
 		err = cmd.Wait()
 		if err != nil {
 			i.Logger.Errorf("importer finished with error: %v", err)
 		}
 		close(stopChan)
+		time.Sleep(10 * time.Second)
 	}
 }
